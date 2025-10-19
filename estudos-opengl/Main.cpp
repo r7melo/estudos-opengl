d770d20270920e74941c9fdd04f1e2c23ca7d99e
Romario Melo <romariomelo360@gmail.com>
2025-10-18 23:05:04 -0300

    Aula sobre texturas, adicionando imagem 2D na figura.

--- estudos-opengl/Main.cpp
+++ estudos-opengl/Main.cpp
@@ -1,28 +1,35 @@
 #include <iostream>        // Biblioteca padrão para entrada e saída (ex: cout)
 #include <glad/glad.h>     // Carrega todas as funções modernas do OpenGL
 #include <GLFW/glfw3.h>    // Gerencia janelas e contexto do OpenGL
+#include <stb/stb_image.h>
 
 #include"shaderClass.h"
+#include"Texture.h"
 #include"VAO.h"
 #include"VBO.h"
 #include"EBO.h"
 
+
+
+
 // Define os vértices do triângulo (posição XYZ)
-GLfloat vertices[] = 
-{//|        Coordenadas                         |       Cores          //  
-    -0.50f, -0.5f * float(sqrt(3)) / 3,     0.0f,   0.0f, 1.0f, 0.0f,  // Inferior esquerdo
-     0.50f, -0.5f * float(sqrt(3)) / 3,     0.0f,   1.0f, 0.0f, 0.0f,  // Inferior direito
-     0.00f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,   0.0f, 0.0f, 1.0f,  // Superior
-    -0.25f,  0.5f * float(sqrt(3)) / 6,     0.0f,   1.0f, 0.0f, 0.0f,  // Meio esquerdo
-     0.25f,  0.5f * float(sqrt(3)) / 6,     0.0f,   0.0f, 1.0f, 0.0f,  // Meio direito
-     0.00f, -0.5f * float(sqrt(3)) / 3,     0.0f,   0.0f, 0.0f, 1.0f,  // Meio Inferior
+GLfloat vertices[] =
+{//|     Coordenadas  |       Cores       | Textura Coord.  //  
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,  // Top Right
+    -0.5f,  0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.0f, 1.0f,  // Top Left
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,  // Bottom Left
+     0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f   // Bottom Right
 };
 
 GLuint indices[] = {
-    0, 3, 5, // Triangulo inferior esquerdo
-    3, 2, 4, // Triangulo inferior direito
-    5, 4, 1  // Triangulo superior
+    0, 1, 2,  // Triângulo 1
+	0, 2, 3  // Triângulo 2 
 };
+
+
+
+
+
 
 int main() {
     // Inicializa a biblioteca GLFW
@@ -55,6 +62,12 @@ int main() {
     // Define a área de renderização (de 0,0 até 800,800)
     glViewport(0, 0, 800, 800);
 
+
+
+
+
+
+
 	Shader shaderProgram("default.vert", "default.frag");
 
 	VAO VAO1;
@@ -63,21 +76,33 @@ int main() {
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
     
-    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	// [0][1][2]-[3][4][5]-[6][7]
+	// Posições |  Cores  | Texturas
+
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale"); // Pega o local do uniforme "scale" no shader
 
-    // Define a cor de fundo (RGBA: azul escuro)
-    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 
-    // Limpa o buffer de cor aplicando a cor definida
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    
+
+
+
+    // Texture 
+
+
+	Texture popCat("C:\\Users\\Romario Melo\\Documents\\estudos-opengl\\docs\\images\\06\\pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	popCat.texUnit(shaderProgram, "tex0", 0);
+
+
+
+
+
 
     // Loop principal — mantém a janela aberta até o usuário fechá-la
     while (!glfwWindowShouldClose(window))
@@ -86,8 +111,11 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT); // Limpa o buffer de cor
 		
 		shaderProgram.Activate(); // Ativa o programa de shader
-		glUniform1f(uniID, 1.0f); // Define o valor do uniforme "scale" para 0.5
-		VAO1.Bind(); // Vincula o VAO (prepara o triângulo para ser desenhado)
+		glUniform1f(uniID, .5f); // Define o valor do uniforme "scale" para 0.5
+
+		popCat.Bind();
+		
+        VAO1.Bind(); // Vincula o VAO (prepara o triângulo para ser desenhado)
 
 		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0); // Desenha o triângulo
 
@@ -102,6 +130,7 @@ int main() {
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	popCat.Delete();
 	shaderProgram.Delete();
 
     // Destroi a janela e libera memória
